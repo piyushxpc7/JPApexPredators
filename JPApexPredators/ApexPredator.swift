c8cb4319f6e80428cc90a3eef6da6e8f9016f326
piyushxpc7 <piyushxpc@gmail.com>
2024-03-02 18:16:44 +0530

    Initial Commit

---
+++ JPApexPredators/ApexPredator.swift
@@ -0,0 +1,8 @@
+//
+//  ApexPredator.swift
+//  JPApexPredators
+//
+//  Created by Chandra on 02/03/24.
+//
+
+import Foundation
---
+++ JPApexPredators/Predators.swift
@@ -0,0 +1,8 @@
+//
+//  Predators.swift
+//  JPApexPredators
+//
+//  Created by Chandra on 02/03/24.
+//
+
+import Foundation
